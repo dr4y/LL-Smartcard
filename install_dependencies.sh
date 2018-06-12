fa425fb0dba2882f10e759746b2a63585cbeb254
Christian Dresen <c.dresen@fh-muenster.de>
2018-06-12 09:44:24 +0200

    Update install_dependencies.sh
    Added -y to apt-get install

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,7 @@ cd pyDes-2.0.1
 sudo python setup.py install
 
 echo "Installing pyscard..."
-sudo apt-get install python-pyscard
+sudo apt-get install -y python-pyscard
 
 echo "Installing PC/SC"
-sudo apt-get install pcsc-tools pcscd
+sudo apt-get install -y pcsc-tools pcscd
